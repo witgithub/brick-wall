a32956ddb11508dc96f3784d94f3abad16be9562
witbitbuck <witold.krysztofiak@globegroup.pl>
2017-02-03 09:31:20 +0100

    importy

---
+++ src/main.js
@@ -0,0 +1,13 @@
+console.log("test: " , document);
+//import {data} from "./helper";
+
+const container = document.querySelector(".container");
+
+
+
+const onInit = function(){
+    
+    container.appendChild(document.createTextNode("kontener"));
+};
+
+onInit();
